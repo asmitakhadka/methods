72083d12ace34751fb6326c20ba440cf49c0e33b
Asmita Khadka <10015309@student.toiohomai.ac.nz>
2018-09-13 15:06:33 +1200

    methods ex

---
+++ question1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a number");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine($"The input (number) chnaged i.e {ChangeSign(number)}");
+        }
+        public static int ChangeSign(int number)
+        {
+            return number *- 1;
+        }
+    }
+}
